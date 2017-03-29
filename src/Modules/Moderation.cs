0de667ffbb4324cbb289db68e1ec97726239cf96
Apple is Void <1piedude11@gmail.com>
2017-03-29 00:36:08 -0600

    Added aliases for kick and ban
    `fuckoff` for ban and `pissoff` for kick

--- src/Modules/Moderation.cs
+++ src/Modules/Moderation.cs
@@ -13,7 +13,7 @@ namespace DEA.Modules
     public class Moderation : ModuleBase<SocketCommandContext>
     {
         [Command("Ban")]
-        [Alias("hammer")]
+        [Alias("hammer", "fuckoff")]
         [RequireModerator]
         [RequireBotPermission(GuildPermission.BanMembers)]
         [Summary("Bans a user from the server.")]
@@ -28,7 +28,7 @@ namespace DEA.Modules
         }
 
         [Command("Kick")]
-        [Alias("boot")]
+        [Alias("boot", "pissoff")]
         [RequireModerator]
         [RequireBotPermission(GuildPermission.KickMembers)]
         [Summary("Kicks a user from the server.")]
