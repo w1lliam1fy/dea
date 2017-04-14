b9dc4bfee55a73244ee01dd92688864073ebb733
Blaze It <realblazeit@gmail.com>
2017-04-13 23:54:58 -0400

    intrestrate increase

--- src/Services/Math.cs
+++ src/Services/Math.cs
@@ -4,8 +4,8 @@
     {
         public static decimal CalculateIntrestRate(decimal wealth)
         {
-            var InterestRate = 0.01m + ((wealth / 100) * .000015m);
-            if (InterestRate > 0.025m) InterestRate = 0.025m;
+            var InterestRate = 0.01m + ((wealth / 100) * .00004m);
+            if (InterestRate > 0.05m) InterestRate = 0.05m;
             return InterestRate;
         }
     }
