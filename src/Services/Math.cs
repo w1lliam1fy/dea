bbcd082d07a15c849052022ec4ab686182f2306e
RealBlazeIt <john.n.boyer@gmail.com>
2017-04-14 13:13:28 -0700

    actualy gangs buff

--- src/Services/Math.cs
+++ src/Services/Math.cs
@@ -4,7 +4,7 @@
     {
         public static decimal CalculateIntrestRate(decimal wealth)
         {
-            var InterestRate = 0.01m + ((wealth / 100) * .00002m);
+            var InterestRate = 0.01m + ((wealth / 100) * .00008m);
             if (InterestRate > 0.05m) InterestRate = 0.05m;
             return InterestRate;
         }
