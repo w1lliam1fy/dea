ab7188f6e1774df9677adc8028becb83dc867c7a
Blaze It <realblazeit@gmail.com>
2017-04-15 23:33:00 -0400

    minor fix

--- src/Modules/General.cs
+++ src/Modules/General.cs
@@ -276,7 +276,7 @@ namespace DEA.Modules
         public async Task Callme([Remainder] string name)
         {
             if (name.Length > 32) Error("Your DEA nickname may not be longer than 32 characters.");
-            name = name.Replace(Environment.NewLine, string.Empty);
+            name = name.Replace("\n", string.Empty);
             await UserRepository.ModifyAsync(Context, x => x.Name, name);
             await Send($"How's it going, *{name}*?");
         }
