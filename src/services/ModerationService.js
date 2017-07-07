78c9dd6b3cec580bcdc9c7efaebb627e29c7d2e4
Blaze It <realblazeit@gmail.com>
2017-07-07 17:29:17 -0500

    correct mod log display

--- src/services/ModerationService.js
+++ src/services/ModerationService.js
@@ -50,7 +50,7 @@ class ModerationService {
     }
 
     if (subject != null) {
-      description += '**User:** ' + subject + ' (' + subject.id + ')\n';
+      description += '**User:** ' + util.StringUtil.boldify(subject.tag) + ' (' + subject.id + ')\n';
     }
 
     if (!util.StringUtil.isNullOrWhiteSpace(reason)) {
