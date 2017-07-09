f09c552b12ad1bcbea7ed8744600b3be2dea438e
RealBlazeIt <RealBlazeIt@users.noreply.github.com>
2017-07-09 11:12:38 -0500

    Update Ranks.js

--- src/commands/general/Ranks.js
+++ src/commands/general/Ranks.js
@@ -11,23 +11,23 @@ class Ranks extends patron.Command {
     });
   }
 
-  async run(context, args) {
-    const dbGuild = await db.guildRepo.getGuild(context.guild.id);
+  async run(msg, args) {
+    const dbGuild = await db.guildRepo.getGuild(msg.guild.id);
     const rankList = dbGuild.roles.rank.sort((a, b) => a.cashRequired - b.cashRequired);
 
     if (dbGuild.roles.rank.length === 0) {
-      return util.Messenger.replyError(context.channel, context.author, 'There are no rank roles yet!');
+      return util.Messenger.replyError(msg.channel, msg.author, 'There are no rank roles yet!');
     }
 
     let description = '';
     for(let i = 0; i < rankList.length; i++) {
-      const rank = context.guild.roles.find((x) => x.id === rankList[i].id);
+      const rank = msg.guild.roles.find((x) => x.id === rankList[i].id);
       console.log(rankList[i]);
 
       description+= util.StringUtil.boldify(rank.name) + ': ' + util.NumberUtil.USD(rankList[i].cashRequired) + '.\n';
     }
 
-    return util.Messenger.reply(context.channel, context.author, description);
+    return util.Messenger.reply(msg.channel, msg.author, description);
   }
 }
 
