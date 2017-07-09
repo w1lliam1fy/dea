164779a70a3dc1996c64a6a4886df614de4e19b3
RealBlazeIt <RealBlazeIt@users.noreply.github.com>
2017-07-09 11:17:45 -0500

    Update ModRoles.js

--- src/commands/general/ModRoles.js
+++ src/commands/general/ModRoles.js
@@ -11,22 +11,22 @@ class ModRoles extends patron.Command {
     });
   }
 
-  async run(context, args) {
-    const dbGuild = await db.guildRepo.getGuild(context.guild.id);
+  async run(msg, args) {
+    const dbGuild = await db.guildRepo.getGuild(msg.guild.id);
     const modRoleList = dbGuild.roles.mod.sort((a, b) => a.permissionLevel - b.permissionLevel);
 
     if (dbGuild.roles.mod.length === 0) {
-      return util.Messenger.replyError(context.channel, context.author, 'There are no mod roles yet!');
+      return util.Messenger.replyError(msg.channel, msg.author, 'There are no mod roles yet!');
     }
 
     let description = '';
     for(let i = 0; i < modRoleList.length; i++) {
-      const rank = context.guild.roles.find((x) => x.id === modRoleList[i].id);
+      const rank = msg.guild.roles.find((x) => x.id === modRoleList[i].id);
       console.log(modRoleList[i]);
       description+= util.StringUtil.boldify(rank.name) + ', Permission Level: ' + (modRoleList[i].permissionLevel) + '.\n';
     }
 
-    return util.Messenger.reply(context.channel, context.author, description);
+    return util.Messenger.reply(msg.channel, msg.author, description);
   }
 }
 
