ec5c8fffc839fab7b9a55aed90376230a4638ac8
Blaze It <realblazeit@gmail.com>
2017-07-09 21:25:25 -0500

    more debug

--- src/services/ChatService.js
+++ src/services/ChatService.js
@@ -48,7 +48,7 @@ class ChatService {
           const split = stolen / 6;
 
           for (const message of collection.values()) {
-            console.log(message.author);
+            console.log(message.author.tag);
             await db.userRepo.findAndModifyCash(msg.dbGuild, msg.guild.member(message.author), split);
 
             gangBangers += util.StringUtil.boldify(message.author.tag) + ', ';
