c43733aede1cefb1468618caefef47bc6f3f98de
Blaze It <realblazeit@gmail.com>
2017-07-10 11:04:46 -0500

    build success?

---
+++ test/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const patron = require('patron.js');
+const discord = require('discord.js');
+const db = require('./database');
+const config = require('./config.json');
+const credentials = require('./credentials.json');
+
+async function initiate() {
+  const client = new discord.Client({ fetchAllMembers: true, messageCacheMaxSize: 5, messageCacheLifetime: 30, messageSweepInterval: 1800, disabledEvents: config.disabledEvents, restTimeOffset: 150 });
+  const registry = new patron.Registry();
+
+  registry.registerDefaultTypeReaders();
+  registry.registerGroupsIn(path.join(__dirname, 'groups'));
+  registry.registerCommandsIn(path.join(__dirname, 'commands'));
+
+  await db.connect(credentials.mongodbConnectionUrl);
+  await client.login(credentials.token);
+}
+
+initiate()  
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
